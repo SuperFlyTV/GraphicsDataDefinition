9360bf4df78f591fad877fbd683cb385269fc99b
Johan Nyman <johan@nytamin.se>
2022-09-18 07:40:08 +0200

    fix bug

--- reference-gui/src/App.jsx
+++ reference-gui/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { GDDGUI } from "./gdd-gui";
+import { examples } from "./examples";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
